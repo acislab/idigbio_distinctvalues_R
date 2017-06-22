7792bafd7cb97a8defdba9446844f9f3f40eba07
saniyasahdev <saniya.sahdev@gmail.com>
2017-06-22 16:21:38 -0400

    Update run_R.sh

--- run_R.sh
+++ run_R.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-<<<<<<< HEAD
+
 export HADOOP_CONF_DIR="/etc/hadoop/conf"
 export HADOOP_USER_NAME="hdfs"
 /opt/spark/latest/bin/spark-submit \
@@ -8,7 +8,4 @@ export HADOOP_USER_NAME="hdfs"
 --driver-memory 1G \
 --total-executor-cores 20 \
 --executor-memory 3G \
-R_Job.R
-=======
-spark-submit r_job.R
->>>>>>> 27f4d622d55ee3edd8be2a4811b937af440ae9cc
+r_job.R
