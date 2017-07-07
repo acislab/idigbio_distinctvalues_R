a2277d0ac9b99da88c409b7e1e736d2368c03f46
saniyasahdev <saniya.sahdev@gmail.com>
2017-07-07 14:20:59 -0400

    Create iDigBio_uniq_csv.R

---
+++ iDigBio_uniq_csv.R
@@ -0,0 +1,17 @@
+Sys.setenv(HADOOP_CONF_DIR="/etc/hadoop/conf")
+Sys.setenv(HADOOP_USER_NAME="hdfs")
+Sys.setenv(SPARK_HOME="/opt/spark/latest")
+library(SparkR, lib.loc = c(file.path(Sys.getenv("SPARK_HOME"), "R", "lib")))
+sc <- sparkR.session(master = "local[*]")
+
+entiredata <- read.df("/guoda/data/idigbio-20170609T073048-1M.parquet")
+
+ColumnName<- colnames(entiredata)
+
+for(i in ColumnName){
+    print(i)
+    query<-paste0("SELECT ",i,",count(*) FROM parquet.`/guoda/data/idigbio-20170609T073048-1M.parquet`GROUP BY ", i)
+    DF<-sql(query)
+    command<-paste0("iDigBio_2017-06-09_10:02_uniq_",i,".csv")
+    write.df(DF,command,"csv")
+}
