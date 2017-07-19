5a7fb1110192088b88156d90c92831c63d0d7816
saniyasahdev <saniya.sahdev@gmail.com>
2017-07-19 14:54:09 -0400

    Create SampleCSV.R

---
+++ SampleCSV.R
@@ -0,0 +1,9 @@
+Sys.setenv(HADOOP_CONF_DIR="/etc/hadoop/conf")
+Sys.setenv(HADOOP_USER_NAME="hdfs")
+Sys.setenv(SPARK_HOME="/opt/spark/latest")
+library(SparkR, lib.loc = c(file.path(Sys.getenv("SPARK_HOME"), "R", "lib")))
+sc <- sparkR.session(master = "local[*]")
+
+CountryDF <- sql("SELECT country,count(*) FROM parquet.`/guoda/data/idigbio-20170609T073048-1M.parquet` GROUP BY country ")
+
+write.df(CountryDF, "../../outputs/idigbio/ATestFlie.csv","csv")
